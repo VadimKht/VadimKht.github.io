1013278fa16abca979ba2ed132936f186a04c5e8
vadiks2003 <vadiks2003@ukr.net>
2024-04-02 15:37:18 +0300

    the cube can be spinned (wrong axis), no index buffer used yet but you can walk and strafe

--- belft/index.js
+++ belft/index.js
@@ -5,11 +5,6 @@ window.addEventListener("load", startup, false);
 let gl = null;
 let glCanvas = null;
 
-// Aspect ratio and coordinate system
-// details
-
-let aspectRatio;
-
 // https://www.alanzucconi.com/wp-content/uploads/2016/02/2D_affine_transformation_matrix.svg_.png
 // https://wikimedia.org/api/rest_v1/media/math/render/svg/8ea4e438d7439b8fa504fb53fd7fafd678007243
 const width = 1920;
@@ -20,23 +15,37 @@ const _top = 1.0;
 const bottom = -1.0;
 const far = 100.0;
 const near = 0.0;
-let angle = 1;
-let view = [Math.cos(angle), 0.0, Math.sin(angle), 0.0,
-            0.0, 1.0, 0.0, 0.0,
-            -Math.sin(angle), 0.0, Math.cos(angle), 0.0,
-            0.0, 0.0, 0.0, 1.0];
-let model = [1.0, 0.0, 0.0, 0.0,
-             0.0, 1.0, 0.0, 0.0,
-             0.0, 0.0, 1.0, 0.0,
-             0.0, 0.0, 0.0, 1.0];
+let angleX = 0;
+let angleY = 0;
+let angleZ = 0;
+
+let cameraX = 0.0;
+let cameraY = 0.0;
+let cameraZ = 0.0;
+let cameraXrot = 0.0;
+let cameraYrot = 0.0;
+let cameraZrot = 0.0;
+
+let movingLeft = false;
+let movingRight = false;
+let movingUp = false;
+let movingDown = false;
+let strafingLeft = false;
+let strafingRight = false;
 
 //ortho
-let projection = [ 2/(right-left), 0.0, 0.0, -((right+left)/(right-left)),
+/*let projection = [ 2/(right-left), 0.0, 0.0, -((right+left)/(right-left)),
                   0.0, 2/(_top-bottom), 0.0, -((_top+bottom)/(_top-bottom)),
                   0.0, 0.0, -2/(far-near), -((far+near)/(far-near)),
-                  0.0, 0.0, 0.0, 1.0]
+                  0.0, 0.0, 0.0, 1.0]*/
 
-console.log(projection);
+const aspectRatio = 400 / 400;
+const fov = 90;
+// perspective
+/*let projection = [1.0/(aspectRatio * Math.tan(fov/2)), 0.0, 0.0, 0.0,
+                  0.0, 1.0/(Math.tan(fov/2)), 0.0, 0.0,
+                  0.0, 0.0, -((far+near)/(far-near)), -((2*far*near)/(far-near)),
+                  0.0, 0.0, -1.0, 1.0];*/
 
 // Vertex information
 
@@ -55,6 +64,65 @@ let uModel;
 let uProjection;
 
 let shaderProgram;
+
+window.addEventListener("keydown", function (event) {
+  if(event.key == "ArrowDown" || event.key == "w" || event.key == "W"){
+    movingDown = true;
+    return;
+  }
+  if(event.key == "ArrowUp" || event.key == "s" || event.key == "S")
+  {
+    movingUp = true;
+    return;
+  }
+  if(event.key == "ArrowLeft" || event.key == "a" || event.key == "A")
+  {
+    movingLeft = true;
+    return;
+  }
+  if(event.key == "ArrowRight" || event.key == "d" || event.key == "D"){
+    movingRight = true;
+    return;
+  }
+  if(event.key == "q" || event.key == "Q"){
+    strafingLeft = true;
+    return;
+  }
+  if(event.key == "e" || event.key == "E"){
+    strafingRight = true;
+    return;
+  }
+  // Cancel the default action to avoid it being handled twice
+}, true);
+
+window.addEventListener("keyup", function (event) {
+  if(event.key == "ArrowDown" || event.key == "w" || event.key == "W"){
+    movingDown = false;
+    return;
+  }
+  if(event.key == "ArrowUp" || event.key == "s" || event.key == "S")
+  {
+    movingUp = false;
+    return;
+  }
+  if(event.key == "ArrowLeft" || event.key == "a" || event.key == "A")
+  {
+    movingLeft = false;
+    return;
+  }
+  if(event.key == "ArrowRight" || event.key == "d" || event.key == "D"){
+    movingRight = false;
+    return;
+  }
+  if(event.key == "q" || event.key == "Q"){
+    strafingLeft = false;
+    return;
+  }
+  if(event.key == "e" || event.key == "E"){
+    strafingRight = false;
+    return;
+  }
+}, true);
 
 function startup(){
     /** @type {HTMLCanvasElement} */
@@ -77,17 +145,118 @@ function startup(){
     ];
 
     shaderProgram = buildShaderProgram(shaderSet);
-    aspectRatio = glCanvas.width / glCanvas.height;
 
     let colorArray = new Float32Array(
       [0.5, 0.4, 0.7, 1.0,
        0.4, 0.5, 0.4, 1.0,
-       0.2, 0.8, 0.5, 1.0]);
+       0.2, 0.8, 0.5, 1.0,
+       0.5, 0.4, 0.7, 1.0,
+       0.4, 0.5, 0.4, 1.0,
+       0.2, 0.8, 0.5, 1.0,
+       0.5, 0.4, 0.7, 1.0,
+       0.4, 0.5, 0.4, 1.0,
+       0.2, 0.8, 0.5, 1.0,
+       0.5, 0.4, 0.7, 1.0,
+       0.4, 0.5, 0.4, 1.0,
+       0.2, 0.8, 0.5, 1.0,
+       0.5, 0.4, 0.7, 1.0,
+       0.4, 0.5, 0.4, 1.0,
+       0.2, 0.8, 0.5, 1.0,
+       0.5, 0.4, 0.7, 1.0,
+       0.4, 0.5, 0.4, 1.0,
+       0.2, 0.8, 0.5, 1.0,
+       0.5, 0.4, 0.7, 1.0,
+       0.4, 0.5, 0.4, 1.0,
+       0.2, 0.8, 0.5, 1.0,
+       0.5, 0.4, 0.7, 1.0,
+       0.4, 0.5, 0.4, 1.0,
+       0.2, 0.8, 0.5, 1.0,
+       0.5, 0.4, 0.7, 1.0,
+       0.4, 0.5, 0.4, 1.0,
+       0.2, 0.8, 0.5, 1.0,
+       0.5, 0.4, 0.7, 1.0,
+       0.4, 0.5, 0.4, 1.0,
+       0.2, 0.8, 0.5, 1.0,
+       0.5, 0.4, 0.7, 1.0,
+       0.4, 0.5, 0.4, 1.0,
+       0.2, 0.8, 0.5, 1.0,
+       0.5, 0.4, 0.7, 1.0,
+       0.4, 0.5, 0.4, 1.0,
+       0.2, 0.8, 0.5, 1.0,
+       0.5, 0.4, 0.7, 1.0,
+       0.4, 0.5, 0.4, 1.0,
+       0.2, 0.8, 0.5, 1.0,
+       0.5, 0.4, 0.7, 1.0,
+       0.4, 0.5, 0.4, 1.0,
+       0.2, 0.8, 0.5, 1.0,
+       0.5, 0.4, 0.7, 1.0,
+       0.4, 0.5, 0.4, 1.0,
+       0.2, 0.8, 0.5, 1.0,
+       0.5, 0.4, 0.7, 1.0,
+       0.4, 0.5, 0.4, 1.0,
+       0.2, 0.8, 0.5, 1.0,
+       ]);
 
     vertexArray = new Float32Array([
-        -0.5, 0.5, 0.0,
-        0.5, 0.5, 0.0,
-        0.5, -0.5, 0.0
+        // forward
+        // top
+        -0.5, 0.5, 0.5,
+        0.5, 0.5, 0.5,
+        0.5, -0.5, 0.5,
+        //bottom
+        -0.5, 0.5, 0.5,
+        -0.5, -0.5, 0.5,
+        0.5, -0.5, 0.5,
+
+        // left
+        // top
+        -0.5, 0.5, -0.5,
+        -0.5, 0.5, 0.5,
+        -0.5, -0.5, 0.5,
+        // bottom
+        -0.5, 0.5, -0.5,
+        -0.5, -0.5, -0.5,
+        -0.5, -0.5, 0.5,
+
+        // right
+        // top
+        0.5, 0.5, -0.5,
+        0.5, 0.5, 0.5,
+        0.5, -0.5, 0.5,
+        // bottom
+        0.5, 0.5, -0.5,
+        0.5, -0.5, -0.5,
+        0.5, -0.5, 0.5,
+
+        // back
+        // top
+        -0.5, 0.5, -0.5,
+        0.5, 0.5, -0.5,
+        0.5, -0.5, -0.5,
+        //bottom
+        -0.5, 0.5, -0.5,
+        -0.5, -0.5, -0.5,
+        0.5, -0.5, -0.5,
+
+      // up
+      // idk
+      -0.5, 0.5, 0.5,
+      -0.5, 0.5, -0.5,
+      0.5, 0.5, -0.5,
+
+      -0.5, 0.5, 0.5,
+      0.5, 0.5, 0.5,
+      0.5, 0.5, -0.5,
+
+      // down
+      -0.5, -0.5, 0.5,
+      -0.5, -0.5, -0.5,
+      0.5, -0.5, -0.5,
+
+      -0.5, -0.5, 0.5,
+      0.5, -0.5, 0.5,
+      0.5, -0.5, -0.5,
+
     ]);
 
     vertexBuffer = gl.createBuffer();
@@ -97,9 +266,7 @@ function startup(){
     colorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, colorArray, gl.STATIC_DRAW);
-
-
-    currentAngle = 0.0;
+    gl.enable(gl.DEPTH_TEST);
 
     animateScene();
 }
@@ -149,13 +316,29 @@ function compileShader(id, type) {
     gl.clear(gl.COLOR_BUFFER_BIT);
   
     gl.useProgram(shaderProgram);
-  
-    angle += 0.01;
-    view = [Math.cos(angle), 0.0, Math.sin(angle), 0.0,
-      0.0, 1.0, 0.0, 0.0,
-      -Math.sin(angle), 0.0, Math.cos(angle), 0.0,
-      0.0, 0.0, 0.0, 1.0];
 
+    const speed = 0.03;
+    if(movingUp) cameraZ += speed;
+    if(movingDown) cameraZ -= speed;
+    if(movingLeft) cameraYrot -= speed;
+    if(movingRight) cameraYrot += speed;
+    if(strafingLeft) cameraX -= speed*1.5;
+    if(strafingRight) cameraX += speed*1.5;
+    angleX = 0.5;
+
+    model = [Math.cos(angleY) * Math.cos(angleZ), Math.cos(angleY) * Math.sin(angleZ), -Math.sin(angleY), 0.0,
+      (Math.sin(angleX) * Math.sin(angleY) * Math.cos(angleZ)) - (Math.cos(angleX) * Math.sin(angleZ)), (Math.sin(angleX) * Math.sin(angleY) * Math.sin(angleZ)) + (Math.cos(angleX) * Math.cos(angleZ)), Math.sin(angleX) * Math.cos(angleY), 0.0,
+      (Math.cos(angleX) * Math.sin(angleY) * Math.cos(angleZ)) + (Math.sin(angleX) * Math.sin(angleZ)), (Math.cos(angleX) * Math.sin(angleY) * Math.sin(angleZ)) - (Math.sin(angleX) * Math.cos(angleZ)), Math.cos(angleX) * Math.cos(angleY), 0.0,
+      0.0, 0.0, -1.0, 1.0];
+    view = [Math.cos(cameraYrot) * Math.cos(cameraZrot), Math.cos(cameraYrot) * Math.sin(cameraZrot), -Math.sin(cameraYrot), 0.0,
+      (Math.sin(cameraXrot) * Math.sin(cameraYrot) * Math.cos(cameraZrot)) - (Math.cos(cameraXrot) * Math.sin(cameraZrot)), (Math.sin(cameraXrot) * Math.sin(cameraYrot) * Math.sin(cameraZrot)) + (Math.cos(cameraXrot) * Math.cos(cameraZrot)), Math.sin(cameraXrot) * Math.cos(cameraYrot), 0.0,
+      (Math.cos(cameraXrot) * Math.sin(cameraYrot) * Math.cos(cameraZrot)) + (Math.sin(cameraXrot) * Math.sin(cameraZrot)), (Math.cos(cameraXrot) * Math.sin(cameraYrot) * Math.sin(cameraZrot)) - (Math.sin(cameraXrot) * Math.cos(cameraZrot)), Math.cos(cameraXrot) * Math.cos(cameraYrot), 0.0,
+      -cameraX, -cameraY, cameraZ, 1.0];
+    
+    projection = [ 2/(right-left), 0.0, 0.0, -((right+left)/(right-left)),
+                      0.0, 2/(_top-bottom), 0.0, -((_top+bottom)/(_top-bottom)),
+                      0.0, 0.0, -2/(far-near), -((far+near)/(far-near)),
+                      0.0, 0.0, 0.0, 1.0]
 
 
     uModel = gl.getUniformLocation(shaderProgram, "model");
@@ -191,7 +374,7 @@ function compileShader(id, type) {
       0,
     );
   
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
+    gl.drawArrays(gl.TRIANGLES, 0, 36);
   
     requestAnimationFrame((currentTime) => {
       animateScene();
