b9c982e0f372d3f4161ad16a437ddff1f2bc8a6e
vadiks2003 <vadiks2003@ukr.net>
2025-11-18 17:25:49 +0200

    letters fixed, characters are not letters anymore, a lot to do

--- 2d_game/rework/engine.js
+++ 2d_game/rework/engine.js
@@ -7,46 +7,59 @@ export class Engine{
 
     shaderProgram;
 
-    vertexBuffer;
-    positionsBuffer;
     indexBuffer;
+
+    // Texture buffers. Possibly should turn these to arrays if quantity gets too big
     textureBuffer;
-
     atlasTextureBuffer
-    atlasTexture;
-    textTextures = [];
-    textPositions = [{
-        name: "HelloworldText",
-        position: [0,0],
-        text: "Hello world.",
-        // uniformdata contains data for all letters
-        uniformdata: []
-    },]
+    tex;
 
-    rectcount = 0;
+    textureTexture;
+    atlasTexture;
+    textPositions = [{
+        name: "Helloworld",
+        position: [0,0],
+        text: "t",
+        size: [256,256],
+        ratio: [1,1],
+
+        // data for all LETTERS
+        uniformdata: this.textData,
+
+        // Vacant is for future, when textures are created through function, instead of always making
+        // new ones all time, if there is one "deleted", it will become not vacant, and can be refilled with new texture
+        // todo: it may make more sense to make it "DELETEDFLAG" to reverse conditional,
+        // because with "vacant" system we have to manually fill the field,
+        // and with having it as flag such as "deleted" it'd be as easy to do as 
+        // if(texture[i].DELFLAG) {
+        // createObject[i]; texture[i].DELFLAG = false; // or undefined
+        // }
+        // don't forget though, that this is raw javascript. It's not blueprint(type), its just initial fill example
+        // MAY also be good idea to instead have special set of indices of "vacant" objects instead of having a whole field
+        // this way if there is too many text textures it's O(1) instead of O(x)
+        // although the problem is - indices may change if i plan to ever limit texture array or cut it down, if there is too
+        // many texts on screen, there will be too many texts in the array, and
+        // i'd probably want to make the array smaller when they're gone for sake of freer memory
+        // perhaps a code that once in a while just pops texture from array if the set of vacant ones contains length-1
+        // (sort set, start counting from last (biggest one), if there is numbers in a row, pop that much of array)
+        // This whole text is TO BE DELETED once i implement AddText() RemoveText() and change SetText() to fit the rest
+        vacant: true
+    },]
 
     // objects data (read shaders/vertex "positions" uniform comment)
     uniformData = [];
-    //  same as uniformData, but UI will be drawn in separate call, so basically data for UI.
-    uniformUIData = [];
-
-    tex;
 
     Scene = {
         GameObjects: []
     };
-
-    verticesInScene = 0;
-
-    cameraPosition = [0, 0];
-
-    collisionPositions = [];
-
+    // After deleting object it will have [3] set to vacant and added to vacantids for quicker search
+    vacantobjectids = [];
+    cameraPosition = [0, 0, 8];
     customframebuffer;
 
     constructor(canvas){
         this.#canvas = canvas;
-        this.#gl = this.#canvas.getContext("webgl2");
+        this.#gl = this.#canvas.getContext("webgl2", {antialias: false});
         if(this.#gl == false) alert("Unable to make webgl2 context.");
     }
     async #GetFile(URL)
@@ -136,17 +149,18 @@ export class Engine{
             }
             return false;
         }
+        // REMINDER: in 3D graphics everything is made out of 3d triangles - balls and rectangles are too
     }
-    // makes texture itself and binds to texture buffer
-    MakeTexture(x=512,y=512){
-        this.tex == null ? this.tex = this.#gl.createTexture() : {};
-        this.#gl.bindTexture(this.#gl.TEXTURE_2D, this.tex);
+    MakeTexture(textureBuffer, textureData = null, x=512,y=512){
+        this.#gl.bindTexture(this.#gl.TEXTURE_2D, textureBuffer);
         this.#gl.texParameteri ( this.#gl.TEXTURE_2D, this.#gl.TEXTURE_MAG_FILTER, this.#gl.NEAREST ) ;
-        this.#gl.texImage2D(this.#gl.TEXTURE_2D, 0, this.#gl.RGBA,x, y, 0, this.#gl.RGBA,this.#gl.UNSIGNED_BYTE,
-            null/*new Uint8Array([255, 0, 0, 255,        0, 255, 0, 255,       0, 0, 255, 255,
-                            255, 255, 0, 255,      0, 255, 255, 255,     255, 0, 255, 255,
-                            128, 255, 0, 255,      255, 128, 0, 255,     0, 255, 128, 255
-            ])*/);
+        if(textureData instanceof Image)
+             this.#gl.texImage2D(this.#gl.TEXTURE_2D, 0, this.#gl.RGBA, this.#gl.RGBA, this.#gl.UNSIGNED_BYTE, textureData);
+        else
+        {
+            textureData == null ? {} : textureData = new Uint8Array(textureData);
+            this.#gl.texImage2D(this.#gl.TEXTURE_2D, 0, this.#gl.RGBA,x, y, 0, this.#gl.RGBA,this.#gl.UNSIGNED_BYTE, textureData);
+        }
         this.#gl.texParameteri(this.#gl.TEXTURE_2D, this.#gl.TEXTURE_MIN_FILTER, this.#gl.LINEAR);
         this.#gl.texParameteri(this.#gl.TEXTURE_2D, this.#gl.TEXTURE_WRAP_S, this.#gl.CLAMP_TO_EDGE);
         this.#gl.texParameteri(this.#gl.TEXTURE_2D, this.#gl.TEXTURE_WRAP_T, this.#gl.CLAMP_TO_EDGE);
@@ -156,12 +170,9 @@ export class Engine{
     CreateBuffers()
     {
         // index buffer
-        this.indexBuffer = this.#gl.createBuffer();
-        this.MakeTexture();
-        
+        this.indexBuffer = this.#gl.createBuffer();        
         // reset
         this.#gl.bindBuffer(this.#gl.ARRAY_BUFFER, null);
-        //this.indexArrayData = [];
     }
 
     AddObject(name = "Object", position = [0,0], rotation = 0, scale = [1, 1], shape="circle", texindex = [0,0], texScale = 1,
@@ -176,12 +187,46 @@ export class Engine{
             size: scale[0],
             active: true
         }
-        const MetaData = [name, obj, coliderdata];
+        const MetaData = [name, obj, coliderdata, false];
+        if(this.vacantobjectids.length > 0)
+        {
+            const id = this.vacantobjectids.pop();
+            this.Scene.GameObjects[id] = MetaData;
+            // console.debug(`Found vacant object at ${id}, swappintg the data !`);
+
+            for(let i = 0; i < 9; i++)
+            {
+                this.uniformData[id*9+i] = obj[i];
+            }
+            return id;
+        }
+        else{
+            this.Scene.GameObjects.push(MetaData);
+            this.uniformData.push(...obj);
+            return this.Scene.GameObjects.length-1;
+        }
+    }   
+
+    AddObjectRaw(name = "Object", data)
+    {
+        if(data.length != 9){
+            alert("addobject with data is not done right");
+            return
+        }
+        const coliderdata = {
+            shape: "circle",
+            // size depending on X for no reason, subject to change
+            size: data[6],
+            active: true
+        }
+        const obj = [...data];
+        const MetaData = [name, obj, coliderdata, false]
         this.Scene.GameObjects.push(MetaData);
 
         this.uniformData.push(...obj);
         return this.Scene.GameObjects.length-1;
-    }   
+    }
+
     textToIndex = new Map([
         ["A",[0,0]],
         ["B",[1,0]],
@@ -248,24 +293,23 @@ export class Engine{
     ])
     // temporarily just change current text letter
     SetText(x,y, letter = "a", size = 1){
-        if(letter.length == 1)
+        if(letter.length <= 1)
         {
             let texindex = this.textToIndex.get(letter);
             if(texindex == undefined) texindex = [11,11];
 
-            this.MakeTexture(256,256);
+            this.MakeTexture(this.tex, null, 256,256);
             this.textData = [0,0, 0, 
                         ...texindex, 1, 
-                        size, size, 0];
+                        1, 1, 0];
             this.textPositions[0] = {
                 name: "Helloworld",
                 position: [x,y],
                 text: letter,
                 size: [256,256],
-                ratio: [1,1],
+                ratio: [size,size],
                 uniformdata: this.textData
             }
-            return;
         }
         else if(letter.length > 1)
         {
@@ -291,60 +335,53 @@ export class Engine{
             }
         
             this.textData = [];
-            let xLet = -1;
+            let xLet = -1+(1/(ParseData.biggestLine+1));
             // for some reason if i set 0 with new lines the bottom part will half-disappepar,
             // however if i set -0.5 it will work fine but without new lines, the text is slightly towards the top...
             // similar thing occurs to X for some reason
-            let yLet = ParseData.newLines <= 1 ? 0 : -0.5;
+            let yLet = ParseData.newLines <= 1 ? 0 : -1+(1/ParseData.newLines);
 
             const SizeTotal=[ParseData.biggestLine*256, ParseData.newLines*256]
-            this.MakeTexture(SizeTotal[0], SizeTotal[1]);
+            this.MakeTexture(this.tex, null, SizeTotal[0], SizeTotal[1]) ;
             for(let i = 0; i < letter.length; i++)
             {
-                let curLetter = this.textToIndex.get(letter[i]);
-                if(curLetter == undefined) curLetter = [11,11];
-                // \n
-                if(curLetter == "Down")
+                if(letter[i] == '\n')
                 {
-                    xLet = -1;
+                    xLet = -1+(1/(ParseData.biggestLine+1));
                     yLet += 1/(ParseData.newLines)*2;
                     continue;
                 }
+                let curLetter = this.textToIndex.get(letter[i]);
+                if(curLetter == undefined) curLetter = [11,11];
+
                 this.textData.push(...[xLet,yLet,0,
                                     ...curLetter,1,
                                     1/ParseData.biggestLine, 1/ParseData.newLines,0
                 ])
-                xLet += 1/ParseData.biggestLine*2;
+                xLet += 1/(ParseData.biggestLine+1)*2;
             }
             this.textPositions[0] = {
                 name: "Helloworld",
                 position: [x,y],
                 text: letter,
                 size: [SizeTotal[0], SizeTotal[1]],
-                ratio: [ParseData.biggestLine, ParseData.newLines],
+                ratio: [ParseData.biggestLine*size, ParseData.newLines*size],
                 uniformdata: this.textData
             }
         }
+        this.DrawTextFrameBuffer();
     }
-
-    AddObjectRaw(name = "Object", data
-    ){
-        if(data.length != 9){
-            alert("addobject with data is not done right");
-            return
-        }
-        const coliderdata = {
-            shape: "circle",
-            // size depending on X for no reason, subject to change
-            size: data[6],
-            active: true
-        }
-        const obj = [...data];
-        const MetaData = [name, obj, coliderdata]
-        this.Scene.GameObjects.push(MetaData);
-
-        this.uniformData.push(...obj);
-        return this.Scene.GameObjects.length-1;
+    ResizeCallback()
+    {
+        const ratio = this.#canvas.width/this.#canvas.height;
+        let matrix;
+        // transform matrix for objects to fit the aspect
+        // [1/ratio, 0]  or  [1,   0  ]
+        // [  0,     1]      [0, ratio]
+        // depending on whether its horizontal or vertical screen (<1 or >1)
+        if(ratio < 1) matrix = [1/ratio, 0.0, 0.0, 1.0];
+        else matrix = [1.0, 0.0, 0.0, ratio];
+        this.#gl.uniformMatrix2fv(this.canvSizeloc, false, matrix);
     }
     FindObjects(name){
         let results = [];
@@ -377,8 +414,9 @@ export class Engine{
             console.error("RemoveObject has been called with incorrect ID");
             return;
         }
-        const er = this.Scene.GameObjects.splice(id, 1);
-        this.uniformData.splice(id*9, 9);
+        this.Scene.GameObjects[id] = 0;
+        this.uniformData.fill(0, id*9, id*9+9);
+        this.vacantobjectids.push(id);
 
     }
     // change object uniformData by index,by specifying id, its index, and data you want to fill
@@ -408,7 +446,6 @@ export class Engine{
     }
 
     async Init(){
-
         await this.#InitShaders();
 
         this.CreateBuffers();
@@ -421,48 +458,72 @@ export class Engine{
         this.atlasTextureBuffer = this.#gl.createTexture();
         this.atlasTexture = await new Image();
         this.atlasTexture.onload = ()=>{
-            this.#gl.bindTexture(this.#gl.TEXTURE_2D, this.atlasTextureBuffer);
-            this.#gl.texParameteri(this.#gl.TEXTURE_2D, this.#gl.TEXTURE_WRAP_S, this.#gl.CLAMP_TO_EDGE);
-            this.#gl.texParameteri(this.#gl.TEXTURE_2D, this.#gl.TEXTURE_WRAP_T, this.#gl.CLAMP_TO_EDGE);
-            this.#gl.texParameteri(this.#gl.TEXTURE_2D, this.#gl.TEXTURE_MIN_FILTER, this.#gl.LINEAR);
-            this.#gl.texParameteri(this.#gl.TEXTURE_2D, this.#gl.TEXTURE_MAG_FILTER, this.#gl.LINEAR);
-            this.#gl.texImage2D(this.#gl.TEXTURE_2D, 0, this.#gl.RGBA, this.#gl.RGBA, this.#gl.UNSIGNED_BYTE, this.atlasTexture);
-            this.#gl.bindTexture(this.#gl.TEXTURE_2D, null);
+            this.MakeTexture(this.atlasTextureBuffer, this.atlasTexture)
+            this.DrawTextFrameBuffer();
         };
-                
+
+        this.tex = this.#gl.createTexture();
+        this.MakeTexture(this.tex,null,512,512);
+        this.textureBuffer = this.#gl.createTexture();
+        this.MakeTexture(this.textureBuffer, [255,0,0,255, 0,255,0,255, 255,0,255,255, 0,255,255,255], 2,2);
+
         this.atlasTexture.src = "AlphabetTex.png"
 
 
-        /*[0.0, 0.0, 0.0,
-                                          0.0, 0.0, 2.0,
-                                          2,    2,   0,
-                                        
-                                        4.0, 0.0, 0.0,
-                                        1.0, 2.0, 1.0,
-                                        2.0, 2.0, 0]*/
+        // Further detail in shaders/vertex at positions uniform.
         this.#gl.uniformMatrix3fv(this.posloc, false, this.uniformData);
 
         // ratio things are done for proper calculation with positive and negative w/h ratios
         const ratio = this.#canvas.width/this.#canvas.height;
         let matrix;
+        // transform matrix for objects to fit the aspect
+        // [1/ratio, 0]  or  [1,   0  ]
+        // [  0,     1]      [0, ratio]
+        // depending on whether its horizontal or vertical screen (<1 or >1)
         if(ratio < 1) matrix = [1/ratio, 0.0, 0.0, 1.0];
         else matrix = [1.0, 0.0, 0.0, ratio];
         this.#gl.uniformMatrix2fv(this.canvSizeloc, false, matrix);
         this.#gl.uniform1f(this.normalizedSpriteSize, 1/12);
-        this.#gl.uniform2fv(this.cameraPosLoc, this.cameraPosition);
+        this.#gl.uniform3fv(this.cameraPosLoc, this.cameraPosition);
+
+        this.#gl.enable(this.#gl.BLEND);
+        this.#gl.blendFunc(this.#gl.SRC_ALPHA, this.#gl.ONE_MINUS_SRC_ALPHA);
 
         // create custom frame buffer and attach this.tex to it, and then bind default framebuffer instead.
         this.customframebuffer = this.#gl.createFramebuffer();
         this.#gl.bindFramebuffer(this.#gl.FRAMEBUFFER, this.customframebuffer);
         // IMPORTANT: frambuffertexture applies texture on thix.tex
         this.#gl.framebufferTexture2D(this.#gl.FRAMEBUFFER, this.#gl.COLOR_ATTACHMENT0, this.#gl.TEXTURE_2D, this.tex, 0);
-        //this.#gl.clearColor(0, 0, 1, 1); 
-        //this.#gl.clear(this.#gl.COLOR_BUFFER_BIT | this.#gl.DEPTH_BUFFER_BIT);
         this.#gl.bindFramebuffer(this.#gl.FRAMEBUFFER, null);
-        //this.#gl.enable(this.#gl.BLEND);
-        //this.#gl.blendFunc(this.#gl.SRC_ALPHA, this.#gl.ONE_MINUS_SRC_ALPHA);
     }
     Draw()
+    {
+        // Use main shader program and write into main framebuffer
+        this.#gl.useProgram(this.shaderProgram);
+        
+        this.#gl.bindFramebuffer(this.#gl.FRAMEBUFFER, null);
+        this.#gl.viewport(0,0, this.#canvas.width,this.#canvas.height);
+
+        // render one texture
+        this.#gl.bindTexture(this.#gl.TEXTURE_2D, this.textureBuffer);
+        this.#gl.uniform1f(this.normalizedSpriteSize, 1/2);
+        const rectcount = this.Scene.GameObjects.length;
+        this.#gl.uniformMatrix3fv(this.posloc, false, this.uniformData);
+        this.#gl.drawElementsInstanced(this.#gl.TRIANGLES, 6, this.#gl.UNSIGNED_SHORT, 0, rectcount);
+
+        this.#gl.bindTexture(this.#gl.TEXTURE_2D, this.tex);
+        this.#gl.uniform1f(this.normalizedSpriteSize, 1);
+        this.#gl.uniformMatrix3fv(this.posloc, false, [this.textPositions[0].position[0], this.textPositions[0].position[1], 0,
+                                                        0,0,1,
+                                                        this.textPositions[0].ratio[0],this.textPositions[0].ratio[1],0]);
+        this.#gl.drawElementsInstanced(this.#gl.TRIANGLES, 6, this.#gl.UNSIGNED_SHORT, 0, 1);
+
+        this.#gl.bindTexture(this.#gl.TEXTURE_2D, null);
+        // This manual way of handling textures and shaderprograms on top of that isn't cool
+    }
+
+    // Draw text into framebuffer
+    DrawTextFrameBuffer()
     {
         // Draw text onto framebuffer (later will be text texture generator) using shader program that doesnt
         // count in for camera and aspect ratio 
@@ -475,36 +536,25 @@ export class Engine{
         this.#gl.clear(this.#gl.COLOR_BUFFER_BIT);
         this.#gl.uniformMatrix3fv(this.texposloc, false, this.textData);
         this.#gl.drawElementsInstanced(this.#gl.TRIANGLES, 6, this.#gl.UNSIGNED_SHORT, 0, this.textData.length/9);
-
-        // Use main shader program and write into main framebuffer
-        this.#gl.useProgram(this.shaderProgram);
-        
-        this.#gl.bindFramebuffer(this.#gl.FRAMEBUFFER, null);
-        this.#gl.viewport(0,0, this.#canvas.width,this.#canvas.height);
-
-        // render one texture
-        this.#gl.bindTexture(this.#gl.TEXTURE_2D, this.atlasTextureBuffer);
-        this.#gl.uniform1f(this.normalizedSpriteSize, 1/12);
-        this.rectcount = this.Scene.GameObjects.length;
-        this.#gl.uniformMatrix3fv(this.posloc, false, this.uniformData);
-        this.#gl.drawElementsInstanced(this.#gl.TRIANGLES, 6, this.#gl.UNSIGNED_SHORT, 0, this.rectcount);
-
-        // render framebuffered texture
-        this.#gl.bindTexture(this.#gl.TEXTURE_2D, this.tex);
-        this.#gl.uniform1f(this.normalizedSpriteSize, 1);
-        this.#gl.uniformMatrix3fv(this.posloc, false, [this.textPositions[0].position[0], this.textPositions[0].position[1], 0,
-                                                        0,0,1,
-                                                        this.textPositions[0].ratio[0],this.textPositions[0].ratio[1],0]);
-        this.#gl.drawElementsInstanced(this.#gl.TRIANGLES, 6, this.#gl.UNSIGNED_SHORT, 0, 1);
-
+                this.#gl.useProgram(this.shaderProgram);
         this.#gl.bindTexture(this.#gl.TEXTURE_2D, null);
-        // This manual way of handling textures and shaderprograms on top of that isn't cool
+        this.#gl.bindFramebuffer(this.#gl.FRAMEBUFFER, null);
     }
-
+    scale_camera(float)
+    {
+        this.cameraPosition[2] = float;
+        this.#gl.uniform3fv(this.cameraPosLoc, this.cameraPosition);
+    }
+    set_camera(vector2)
+    {
+        this.cameraPosition[0] = vector2.x;
+        this.cameraPosition[1] = vector2.y;
+        this.#gl.uniform3fv(this.cameraPosLoc, this.cameraPosition);
+    }
     move_camera(vector2)
     {
         this.cameraPosition[0] += vector2.x;
         this.cameraPosition[1] += vector2.y;
-        this.#gl.uniform2fv(this.cameraPosLoc, this.cameraPosition);
+        this.#gl.uniform3fv(this.cameraPosLoc, this.cameraPosition);
     }
 }
--- 2d_game/rework/index.js
+++ 2d_game/rework/index.js
@@ -4,15 +4,13 @@ import { gameVariables } from "./gamevariables.js";
 
 const canvas = document.getElementById("canvas");
 
-// Bug: doesnt actually find correct height...
-canvas.width = document.body.clientWidth;
-canvas.height = document.body.clientHeight;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 const rectpos = canvas.getBoundingClientRect();
 let engine = new Engine(canvas);
 
-const ratio = canvas.width/canvas.height;
-
+let ratio = canvas.width/canvas.height;
 
 engine.AddObjectRaw("player", [0.0, 0.0, 0.0,
     0.0, 0.0, 1.0,
@@ -44,21 +42,26 @@ document.MoveCamera = (vector2) => {
 
 let pointerid = 0;
 
-engine.SetText(0,0, "ab\ncd", 1);
+engine.SetText(0,0, "Current problem in code:\nVacant system on\n removing Is BROKEN!", 0.5);
 const beginning = "A".charCodeAt();
 document.onmousedown = (e)=>{
 
-    engine.SetText(0,0, String.fromCharCode(pointerid+beginning), 1);
-    pointerid++;
-
+    //engine.SetText(0,0, String.fromCharCode(pointerid+beginning), 1);
+    //pointerid++;
+    engine.AddObject("Bullet", [engine.Scene.GameObjects[0][1][0], engine.Scene.GameObjects[0][1][1]], wtf({x: engine.Scene.GameObjects[0][1][0], y: engine.Scene.GameObjects[0][1][1]}));
     shouldDraw = true;
-
+    
 };
 
 let touchControls = false;
 let touchOrigin = [0,0];
 let touchDragged = [0,0];
 let touchStartTime = 0;
+let collisionObjects = {
+    name: "SampleName",
+    ref: undefined,
+
+}
 document.ontouchstart = (e) => {
     const PointCentre = FindPointCentre(e.touches[0].pageX, e.touches[0].pageY, false);
     touchControls = true;
@@ -85,28 +88,96 @@ document.ontouchend = (e) =>{
     if(new Date().getTime()-touchStartTime < 100) KeysPressed.e = 1;
 }
 
-let KeysPressed = {"w": 0, "a": 0, "s": 0, "d": 0, "e": 0, "esc": 0};
+let scale = 1;
+let KeysPressed = {"w": 0, "a": 0, "s": 0, "d": 0, "e": 0, "esc": 0, "-": 0};
+let KeysTapped = {...KeysPressed};
+function UpdateCameraBoundary(){
+    const padding = {x: 2, y:2}
+    cameraBoundary = ratio > 1 ? [engine.cameraPosition[2]-padding.x, engine.cameraPosition[2]/ratio-padding.y] : [engine.cameraPosition[2]*ratio-padding.x, engine.cameraPosition[2]-padding.y];
+}
 document.onkeydown = (e)=>{
-    if(e.code == "KeyW")  KeysPressed.w = 1;
-    if(e.code == "KeyA")  KeysPressed.a = 1;
-    if(e.code == "KeyS")  KeysPressed.s = 1;
-    if(e.code == "KeyD")  KeysPressed.d = 1;
-    if(e.code == "KeyE")  KeysPressed.e = 1;
-    if(e.code == "Escape") isRunning = false;
+    switch(e.code)
+    {
+        case "KeyW":
+            KeysPressed.w = 1;
+            break;
+        case "KeyA":
+            KeysPressed.a = 1;
+            break;
+        case "KeyS":
+            KeysPressed.s = 1;
+            break;
+        case "KeyD":
+            KeysPressed.d = 1;
+            break;
+        case "KeyE":
+            if(KeysPressed.e == 0) KeysTapped.e = 1;
+            KeysPressed.e = 1;
+            break;
+        case "ShiftLeft":
+            characterSpeed = 10;
+            break;
+        case "Escape":
+            engine.set_camera({x:engine.Scene.GameObjects[0][1][0],y:engine.Scene.GameObjects[0][1][1]});
+            engine.SetText(engine.Scene.GameObjects[0][1][0], engine.Scene.GameObjects[0][1][1], "Game stopped.\nReload page\n to resume.")
+            shouldDraw = true;
+            isRunning = false;
+            break;
+        case "Minus":
+            engine.scale_camera(scale);
+            scale++;
+            UpdateCameraBoundary();
+            shouldDraw = true;
+            break;
+        case "Space":
+
+            break;
+        default:
+            //console.log(`Unknown key ${e.code} pressed.`)
+            break;
+    }
 }
 document.onkeyup = (e)=>{
-    if(e.code == "KeyW")  KeysPressed.w = 0;
-    if(e.code == "KeyA")  KeysPressed.a = 0;
-    if(e.code == "KeyS")  KeysPressed.s = 0;
-    if(e.code == "KeyD")  KeysPressed.d = 0;
-    if(e.code == "KeyE")  KeysPressed.e = 0;
+    switch(e.code)
+    {
+        case "KeyW":
+            KeysPressed.w = 0;
+            break;
+        case "KeyA":
+            KeysPressed.a = 0;
+            break;
+        case "KeyS":
+            KeysPressed.s = 0;
+            break;
+        case "KeyD":
+            KeysPressed.d = 0;
+            break;
+        case "KeyE":
+            KeysPressed.e = 0;
+            break;
+        case "ShiftLeft":
+            characterSpeed = 5;
+            break;
+        default:
+            break;
+    }
 }
-
+window.onresize = () =>{
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    ratio = canvas.width/canvas.height;
+    UpdateCameraBoundary();
+    engine.ResizeCallback();
+    shouldDraw = true;
+}
 // screen ratio issues
 // bigger bug found: under bigger/smaller resolution the object spawns slightly lower then needed and camera is calculated tihs way too
-const cameraBoundary = ratio > 1 ? [6, 6/ratio] : [6*ratio, 6];
+let cameraBoundary;
+UpdateCameraBoundary();
 
 let characterSpeed = 5;
+
+let talkedToNPC = false;
 
 // delta time calculation vraibles
 let prevTime = new Date();
@@ -122,6 +193,7 @@ function Update()
     ar = (nextTime.getTime() - prevTime.getTime()) / 1000;
     prevTime = nextTime;
 
+    
     if(KeysPressed.w == 1 || KeysPressed.a == 1 ||
         KeysPressed.s == 1 || KeysPressed.d == 1)
     {
@@ -142,32 +214,32 @@ function Update()
         }
     }
 
-
     // temporary code for finding colissions, for now finds collisions between player and all objects in scene and deletes by default
     // and in other cases does interaction or remove on interaction key.
-    if(engine.Scene.GameObjects.length > 1)
+    if(KeysTapped.e && engine.Scene.GameObjects.length > 1)
         for(let i = 1; i < engine.Scene.GameObjects.length; i++)
         {
-            if(engine.CheckCollision(engine.Scene.GameObjects[0], engine.Scene.GameObjects[i]))
+            if(!engine.vacantobjectids.includes(i) && engine.CheckCollision(engine.Scene.GameObjects[0], engine.Scene.GameObjects[i]))
             {
                 if(engine.Scene.GameObjects[i] == engine.Scene.GameObjects[myNPC])
                 {
-                    if(KeysPressed.e) {
-                        engine.SetText(5,1.5, "Hello i'm NPC.\nNew line test\nThe text is weird.\nShould probably\nrewrite whole thing")
+                    if(!talkedToNPC) {
+                        engine.SetText(6, 1.5, "Hello i'm NPC.\nNew line test\nThe text is weird.\nShould probably\nrewrite whole thing", 0.4)
                         shouldDraw = true;
+                        talkedToNPC = true;
                     }
                     continue;
                 }
-                if(engine.Scene.GameObjects[i][0] == "e")
-                {
-                    if(KeysPressed.e)
-                    {
+                switch(engine.Scene.GameObjects[i][0]){
+                    case "e":
                         engine.RemoveObject(i);
                         shouldDraw = true;
-                    }
-                    continue
+                        break;
+                    default:
+                        engine.RemoveObject(i);
+                        shouldDraw = true;
+                        break;
                 }
-                engine.RemoveObject(i);
             }
         }
 
@@ -194,6 +266,8 @@ function Update()
         engine.move_camera({x:0, y:-(characterSpeed * ar)});
     }
 
+    if(KeysTapped.e) KeysTapped.e = 0;
+
     if(shouldDraw)
     {
         engine.Draw();
@@ -211,7 +285,7 @@ document.onmousemove = () => {
 function FindPointCentre(x,y, CountCamera = true)
 {
     // Magic number is camera zoom multiplied by 2 due to both left right sides
-    const magicNumber = 16;
+    const magicNumber = engine.cameraPosition[2]*2;
     // in X multiply by ratio if ratio is above 1, in Y divide by ratio if its above 1 for accuracy
     // how does it work? god knows
     return [((x-rectpos.x-canvas.width/2)/canvas.width*magicNumber)* (ratio < 1 ? ratio : 1) + (CountCamera ? engine.cameraPosition[0] : 0), 
